40330702a0a5861736db157a83a3b2b0b6a7078f
jyocj46 <89291658+jyocj46@users.noreply.github.com>
2022-09-02 23:45:25 -0600

    Add files via upload

---
+++ CrudSQL/src/modelo/Conexion.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author yoc91
+ */
+public class Conexion {
+    public Connection conexionBD;
+    
+    private final String puerto= "3306";
+    private final String bd= "db_empresa_2022";
+    private final String urlConexion = String.format("jdbc:mysql://localhost:%s/%s?serverTimezone=UTC",puerto, bd);
+    private final String usuario = "root";
+    private final String contra = "12345";
+    private final String jdbc ="com.mysql.cj.jdbc.Driver";
+ 
+    public void abrir_conexion(){
+            try{
+                Class.forName(jdbc);
+                conexionBD = DriverManager.getConnection(urlConexion,usuario,contra);
+                //JOptionPane.showMessageDialog(null,"Conexion Exitosa","Exito",JOptionPane.INFORMATION_MESSAGE);
+                
+            }catch(ClassNotFoundException | SQLException ex){
+                    System.out.println("Error: " + ex.getMessage());
+            }
+    }
+    
+    public void cerrar_conexion(){
+        try{
+            conexionBD.close();
+        }catch(SQLException ex){
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    
+}
---
+++ CrudSQL/src/vista/main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author yoc91
+ */
+public class main {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frm_cliente frm = new frm_cliente();
+       frm.show();
+       
+    }
+}
